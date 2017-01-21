6e9fa13858c2e975aa6a754f9082433bd6a4b1a2
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-01-21 13:28:55 +0200

    validate the model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-
+  validates :email, :password, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: { with: /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/, message: 'Only emails allowed' }
+  validates :password, length: { in: 6..20 }
+  validates :password, confirmation: true
 end
