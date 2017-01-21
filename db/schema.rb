94d899dff806512bf6e023b27de4efa5265552da
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-01-21 13:25:30 +0200

    schema redone

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 20170115145422) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
-    t.string   "login"
     t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
