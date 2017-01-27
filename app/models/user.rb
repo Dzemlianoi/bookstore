f9009862c877a522595f2e7dea44fddf8d26afca
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-01-27 13:51:45 +0200

    first step

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,4 @@
 class User < ApplicationRecord
-  validates :email, :password, presence: true
-  validates :email, uniqueness: true
-  validates :email, format: { with: /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/, message: 'Only emails allowed' }
-  validates :password, length: { in: 6..20 }
-  validates :password, confirmation: true
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
