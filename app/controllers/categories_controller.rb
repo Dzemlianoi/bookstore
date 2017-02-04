9a3135ac70cca583f687e693ebb9d80b2b8f289f
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-02-04 19:25:45 +0200

    ref

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
   def show
     case params[:id]
-      when Category.default_category.id.to_s then redirect_to root_path
+      when Category.default_category.id.to_s then redirect_to root_path and return
       when 'default' then @category = Category.default_category
       else @category = Category.find_by_id(params[:id].to_s)
     end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,7 @@
 class Author < ApplicationRecord
   has_many :books
+
+  def full_name
+    "#{self.name} #{self.surname}"
+  end
 end
