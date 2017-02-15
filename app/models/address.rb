80cca2b3d99a77def5130c28cd324b76e70d3593
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-02-15 16:34:37 +0200

    validation added

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,5 +3,9 @@ class Address < ApplicationRecord
   enum kind: [:billing, :shipping]
 
   validates_presence_of :first_name, :last_name, :address, :city, :zip, :country, :phone, :kind
+  validates :phone, :uniqueness => {
+      :scope => [:addressable_type]
+  }
+  validates_inclusion_of :kind, in: Address.kinds.keys
   validates :first_name, length:  { maximum: 20 }
 end
