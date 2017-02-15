7a22319ae6e28e321dc8370ac5e63f4205861f72
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-02-15 15:14:39 +0200

    some validators added

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :addressable, polymorphic: true
-  enum type: [:billing, :shipping]
-  validates :first_name, :last_name, :address, :city, :zip, :country, :phone, :type, presence: true
+  enum kind: [:billing, :shipping]
+
+  validates_presence_of :first_name, :last_name, :address, :city, :zip, :country, :phone, :kind
+  validates :first_name, length:  { maximum: 20 }
 end
