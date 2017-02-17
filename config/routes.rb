35a0b4c7587d13e009e20271aadfdb321b8b0572
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-02-17 23:57:00 +0200

    small ref

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     resources :reviews, only: :create
   end
   resource :addresses
-  resource :order_item, only: [:create, :destroy]
+  resource :order_item, only: [:create, :destroy, :update]
   resources :order
 
 
