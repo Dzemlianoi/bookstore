e6422ac5cc710dff8bc1f6c38c3f0d5d16e19932
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-02-17 16:03:24 +0200

    small ref

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -1,20 +1,30 @@
-$(document).ready(function(){
-    $('.star-cb-group label').on("click", function(){
+var books = {
+
+    init: function(){
+        $('.star-cb-group label').click(this.reviewChecker);
+        $('.input-link').click(this.quantitySwitcher)
+    },
+    reviewChecker: function(){
         $('.star-cb-group input').attr('checked', false);
         $(this).prev().attr('checked', true)
-    });
-
-    $('.input-link').on('click', function(){
+    },
+    quantitySwitcher: function(){
         if (($(this).find('.fa-plus').length)) {
-            $('#book_quantity').val( function(i, oldval) {
-                var int_val = parseInt(oldval);
+            $('#order_item_quantity').val( function(i, old_val) {
+                var int_val = parseInt(old_val);
                 return isNaN(int_val) ? 1 : ++int_val;
             });
         }else{
-            $('#book_quantity').val( function(i, oldval) {
-                var int_val = parseInt(oldval);
+            $('#order_item_quantity').val( function(i, old_val) {
+                var int_val = parseInt(old_val);
                 return (isNaN(int_val) || int_val <= 1 ) ? 1 : --int_val;
             });
         }
-    })
+    }
+};
+
+$(document).ready(function(){
+    books.init();
 });
+
+
