ec330445c297b3d62c1c21d3b9217a7c170f3e2b
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-02-20 15:33:48 +0200

    deliveries updated

--- app/assets/javascripts/order_steps.js
+++ app/assets/javascripts/order_steps.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
+    var total_price_elem = $('tr.total-price .pricable');
+    var old_delivery_elem = $('input[name = delivery]:checked');
+    var total_delivery_price_elem = $('tr.delivery-price .pricable');
+    var old_total_price = +total_price_elem.text().slice(1);
+
     $('.checkbox-icon').click(function(){
         $('.shipping-fields').toggle();
     });
+
+    $('.radio-label').click(function(){
+        var old_delivery_price = old_delivery_elem.length
+            ? old_delivery_elem.closest('tr').find('.price').text().slice(1)
+            : 0;
+        var new_delivery_price = +$(this).closest('tr').find('.price').text().slice(1);
+        total_delivery_price_elem.text('€' + new_delivery_price);
+        total_price_elem.text('€' + (old_total_price - old_delivery_price + new_delivery_price));
+    })
 });
