27659b22515561196ffb5175fa89027894f8df70
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-02-24 00:09:42 +0200

    user try

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,7 @@ class User < ApplicationRecord
   has_one  :image, as: :imageable, dependent: :destroy
   accepts_nested_attributes_for :image
 
-  scope :verified ,-> {}
-
+  scope  :without_provider,      -> { where(provider: nil, uid: nil) }
   devise :database_authenticatable,
          :registerable,
          :recoverable,
@@ -18,12 +17,39 @@ class User < ApplicationRecord
          :omniauthable, :omniauth_providers => [:facebook]
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      generated_password = Devise.friendly_token[0, 20]
-      user.password = generated_password
-      user.skip_confirmation!
-      UserMailer.facebook_reg(user, generated_password).deliver
+    @auth = auth
+    # byebug
+    if !where(email: @auth.info.email).without_provider.first.nil?
+      user = where(email: @auth.info.email).without_provider.first
+      user.update_attributes(provider: @auth.provider, uid: @auth.uid)
+      save_avatar(user)
+      user
+    else
+      where(provider: @auth.provider, uid: @auth.uid).first_or_create do |user|
+        user.email = auth.info.email
+        generated_password = Devise.friendly_token[0, 20]
+        user.password = generated_password
+        save_avatar(user)
+        user.skip_confirmation!
+        UserMailer.facebook_reg(user, generated_password).deliver_later if user.email
+      end
     end
+  end
+
+  def self.save_avatar(user)
+    user.build_image
+    user.image.remote_attachment_url = @auth.info.image.gsub('http://','https://')
+  end
+
+  def already_signuped_with_email
+    where(email: @auth.info.email).without_provider.first
+  end
+
+  def email_required?
+    super && provider.blank?
+  end
+
+  def password_required?
+    super && provider.blank?
   end
 end
