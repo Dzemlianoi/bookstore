e6eb99d052ee4fbd625b08c82879ae69af9b125c
Denis Zemlianoi <dzemlianoi@gmail.com>
2017-04-01 10:25:13 +0300

    images resorted

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,6 @@
 class Image < ApplicationRecord
   belongs_to :imageable, polymorphic: true
   mount_uploader :attachment, AttachmentsUploader
+
+  scope :newest,    -> (num) { order('created_at ASC').limit(num) }
 end
